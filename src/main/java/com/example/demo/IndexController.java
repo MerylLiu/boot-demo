41fd19b5450f607a959e504ebb51d381a39df935
Meryl <aa5151521@126.com>
2020-03-05 18:30:24 +0800

    update

---
+++ src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public Map index() {
+        Map map = new HashMap<>();
+        map.put("test", "Hello world");
+        return map;
+    }
+}
